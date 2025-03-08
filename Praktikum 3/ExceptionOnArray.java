513ff644b060065b0a1a35d7ee45bf7ca06ca7a9
fesst <154745501+ricoaldy-e@users.noreply.github.com>
2025-03-08 22:44:46 +0700

    Create ExceptionOnArray.java

---
+++ Praktikum 3/ExceptionOnArray.java
@@ -0,0 +1,22 @@
+// Nama     : Rico Aldy Kusuma
+// NIM      : 24060123120041
+/**
+ * File      : ExceptionOnArray.java
+ * Deskripsi : Program penggunaan eksepsi menggunakan class library Java
+ *
+ */
+
+ public class ExceptionOnArray{
+    public static void main(String[] args){
+        // instansiasi object array integer
+        Integer[] arrayInteger = new Integer[4];
+        try{
+            arrayInteger[2] = 11;
+            arrayInteger[4] = 10;
+        }catch(ArrayIndexOutOfBoundsException exception){
+            exception.printStackTrace();
+        }finally{
+            System.out.println("Clean up code...");
+        }
+    }
+}
