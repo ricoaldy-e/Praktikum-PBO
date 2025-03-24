76ce71ffd6f1faf16ac1e27ed6933df6596a73fd
fesst <154745501+ricoaldy-e@users.noreply.github.com>
2025-03-24 22:55:10 +0700

    Create Pajak.java

---
+++ Praktikum 5/LATIHAN/Pajak.java
@@ -0,0 +1,8 @@
+/* Nama File    : Pajak.java */
+/* Deskripsi    : Interface Pajak berisi deklarasi method untuk menghitung pajak */
+/* Pembuat      : Rico Aldy Kusuma / 24060123120041 */
+/* Tanggal      : 24 Maret 2025 */
+
+public interface Pajak {
+    double hitungPajak();
+}
