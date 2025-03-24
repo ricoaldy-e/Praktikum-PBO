ba88c884780f6fedcc7fa1522b88c73101561ed5
fesst <154745501+ricoaldy-e@users.noreply.github.com>
2025-03-24 22:54:10 +0700

    Create Pengusaha.java

---
+++ Praktikum 5/LATIHAN/Pengusaha.java
@@ -0,0 +1,46 @@
+/* Nama File    : Pengusaha.java */
+/* Deskripsi    : berisi atribut dan method dalam class Pengusaha */
+/* Pembuat      : Rico Aldy Kusuma / 24060123120041 */
+/* Tanggal      : 24 Maret 2025 */
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Pengusaha extends Manusia implements Pajak {
+    private String npwp;
+    private static int counterPengusaha = 0;
+
+    public Pengusaha(String nama, String tgl_mulai_kerja, String alamat, double pendapatan, String npwp) {
+        super(nama, tgl_mulai_kerja, alamat, pendapatan);
+        this.npwp = npwp;
+        counterPengusaha++;
+    }
+
+    public static int getCounterPengusaha() {
+        return counterPengusaha;
+    }
+
+    public String getNpwp() {
+        return npwp;
+    }
+
+    public void setNpwp(String npwp) {
+        this.npwp = npwp;
+    }
+
+    @Override
+    public int hitungMasaKerja() {
+        return (int) ChronoUnit.YEARS.between(tgl_mulai_kerja, LocalDate.now()) + 4; // B = 4 (digit ke-13 NIM)
+    }
+
+    @Override
+    public double hitungPajak() {
+        return 0.15 * pendapatan;
+    }
+
+    @Override
+    public void cetakInfo() {
+        super.cetakInfo();
+        System.out.println("NPWP                    : " + npwp);
+    }
+}
