4f30ddc9ac7590be42d1a621bdaa58b5f512be9b
Rico Aldy Kusuma <154745501+ricoaldy-e@users.noreply.github.com>
2025-05-11 23:43:52 +0700

    Create BangunDatar.java

---
+++ Praktikum 8/PBO08/3.4/BangunDatar.java
@@ -0,0 +1,9 @@
+/* Nama File    : BangunDatar.java */
+/* Deskripsi    : Kelas abstrak untuk bangun datar */
+/* Pembuat      : Rico Aldy Kusuma / 24060123120041 */
+/* Tanggal      : 11 Mei 2025 */
+
+public abstract class BangunDatar {
+    public abstract double hitungLuas();
+    public abstract double hitungKeliling();
+}
